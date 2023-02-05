b287471c5af5ef45069b788b8cee778493f7972d
SHASHANK <shashank7016204503@gmail.com>
2023-02-05 07:17:06 +0530

    Create Build

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/oY4FW4QpekSCerWuYoz0P6DZe6speUmh
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+module.exports={
   solidity: "0.8.17",
-};
+  networks:{
+    goerli:{
+      url:`https://eth-goerli.g.alchemy.com/v2/6QgoEhHO2-UndztQh11CcCDntFjyUmEh`,
+      accounts:["0x5615b90c5fdd29c1d63b7aed657a6c320efb0aacd48414ec99350ef8bba1c61d"]
+    }
+  }
+}
